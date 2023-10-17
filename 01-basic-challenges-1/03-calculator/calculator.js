0f262ded74d4396e7a3a499d8daedefeb08633dc
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-10-17 20:35:15 +0600

    third  problem done

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,23 @@
-function calculator() {}
+function calculator(num1, num2, op) {
+  if (op === "+") {
+    return num1 + num2;
+  } else if (op === "-") {
+    return num1 - num2;
+  } else if (op === "*") {
+    return num1 * num2;
+  } else if (op === "/") {
+    return num1 / num2;
+  }
+}
+
+console.log(calculator(1, 2, "+"));
+console.log(calculator(10, 5, "-"));
+console.log(calculator(2, 2, "*"));
+console.log(calculator(10, 5, "/"));
+
+// calculator(1, 2, "+"); // 3
+// calculator(10, 5, "-"); // 5
+// calculator(2, 2, "*"); // 4
+// calculator(10, 5, "/"); // 2
 
 module.exports = calculator;
