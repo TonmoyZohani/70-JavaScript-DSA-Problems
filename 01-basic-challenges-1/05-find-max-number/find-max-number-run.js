85f39f9b30a5237ca81d6f9074d0b3f38c50813f
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-10-17 22:27:53 +0600

    fifth problem done

--- 01-basic-challenges-1/05-find-max-number/find-max-number-run.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number-run.js
@@ -1,5 +1,15 @@
-const findMaxNumber = require('./find-max-number');
+const findMaxNumber = (arr) => {
+  let max = arr[0];
 
-const result = findMaxNumber([2, 1, 9, 16, 10]);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+};
+
+const result = findMaxNumber([1, 2, 3, 4, 5, 10, 9, 8, 7, 6]);
 
 console.log(result);
