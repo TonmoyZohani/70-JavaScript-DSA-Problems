e6374034d225c15c5d5bf7f9dd4a711d5e672653
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-10-25 21:59:20 +0600

    tenth problem done

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,9 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  let word = new Set(arr);
 
-module.exports = removeDuplicates;
+  return [...word];
+}
+
+console.log(removeDuplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(removeDuplicates([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
+console.log(removeDuplicates([1, 2, 3, 4, 5, true, 1, 'hello', 2, 3, 'hello', true]));
