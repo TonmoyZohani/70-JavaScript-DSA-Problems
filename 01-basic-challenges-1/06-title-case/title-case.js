177291a971b8ff212cf3a7767f466bbf72aebaec
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-10-25 21:03:57 +0600

    sixth problem done

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,11 @@
-function titleCase() {}
+function titleCase(str) {
+  let strArr = str.toUpperCase().split(" ");
+  // let title = [];
 
-module.exports = titleCase;
+  const title = strArr.map((item) => {
+    return item.charAt(0).toUpperCase() + item.slice(1).toLowerCase();
+  });
+  return title.join(" ");
+}
+
+console.log(titleCase("I'm a little tea pot"));
