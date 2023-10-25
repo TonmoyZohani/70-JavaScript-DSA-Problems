b6e01499b5dda4a6413d506ed82689c7dadc8438
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-10-25 21:37:26 +0600

    ninth problem done

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,23 @@
-function countVowels() {}
+function countVowels(str) {
+  let word = str.toLowerCase();
 
-module.exports = countVowels;
+  let cnt = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    if (
+      word[i] === "a" ||
+      word[i] === "e" ||
+      word[i] === "i" ||
+      word[i] === "o" ||
+      word[i] === "u"
+    ) {
+      cnt++;
+    }
+  }
+
+  return cnt;
+}
+
+console.log(countVowels("hello"));
+console.log(countVowels("why"));
+console.log(countVowels("mississippi"));
