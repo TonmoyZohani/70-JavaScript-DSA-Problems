ad6ee33690552573dcc4221e0921f3b6a5382a3e
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-10-25 21:29:11 +0600

    eighth problem done

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,18 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  let reverse = "";
 
-module.exports = isPalindrome;
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str.charAt(i);
+  }
+
+  if (str === reverse) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("hello"));
+console.log(isPalindrome(""));
