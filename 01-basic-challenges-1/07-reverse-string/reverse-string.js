43ba12941c35d4d8abf4f9b72d376f5599f508a8
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-10-25 21:19:50 +0600

    seventh problem done

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,14 @@
-function reverseString() {}
+function reverseString(str) {
+  //   return str.split("").reverse().join('');
+  let reverse = "";
 
-module.exports = reverseString;
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str.charAt(i);
+  }
+
+  return reverse;
+}
+
+console.log(reverseString("hello"));
+console.log(reverseString("world"));
+console.log(reverseString(""));
