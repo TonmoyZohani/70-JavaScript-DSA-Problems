b87ffa57ee5fc36c32803c411746cad401e62fbd
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-10-26 23:30:16 +0600

    twelveth problem done

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,15 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  let arr = [];
 
-module.exports = arrayIntersection;
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        arr.push(arr1[i]);
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(arrayIntersection([1, 2, 3, 4, 5], [1, 3, 5, 7, 9]));
