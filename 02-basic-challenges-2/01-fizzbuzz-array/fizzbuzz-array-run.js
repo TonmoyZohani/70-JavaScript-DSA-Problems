7a15ceabf582e7445cc0ca79c95c0e754dbce71f
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-10-26 23:12:14 +0600

    eleventh problem done

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array-run.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array-run.js
@@ -1,5 +1,19 @@
-const fizzBuzzArray = require('./fizzbuzz-array');
+const fizzBuzzArray = (num) => {
+  const arr = [];
 
-const result = fizzBuzzArray(15);
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      arr.push("Fizz");
+    } else if (i % 5 === 0) {
+      arr.push("Buzz");
+    } else {
+      arr.push(i);
+    }
+  }
 
-console.log(result);
+  return arr;
+};
+
+console.log(fizzBuzzArray(5));
