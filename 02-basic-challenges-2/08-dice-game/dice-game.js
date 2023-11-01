ed6bbe6f0df6c547023774d5008bf6e3d2862009
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-01 23:01:07 +0600

    dice  problem done

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,22 @@
-function diceGameSimulation() {}
+function diceGameSimulation(sim) {
+  let arr = [];
 
-module.exports = diceGameSimulation;
+  for (i = 0; i < sim; i++) {
+    let obj = { dice1: "", dice2: "", sum: "", result: "" };
+    obj.dice1 = Math.floor(Math.random() * 6 + 1);
+    obj.dice2 = Math.floor(Math.random() * 6 + 1);
+    obj.sum = obj.dice1 + obj.dice2;
+    obj.result =
+      obj.sum === 7 || obj.sum === 11
+        ? "win"
+        : obj.sum === 2 || obj.sum === 3 || obj.sum === 12
+        ? "lose"
+        : "roll again";
+
+    arr.push(obj);
+  }
+
+  return arr;
+}
+
+console.log(diceGameSimulation(3));
