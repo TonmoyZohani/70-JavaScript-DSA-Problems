4ec05913bd700f2c34130f7c19a8a6b42b031d3c
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-03 20:41:03 +0600

    find missing letter done

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,14 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-module.exports = findMissingLetter;
+  for (let i = 0; i < arr.length; i++) {
+    const currIndex = alphabet.indexOf(arr[i]);
+    const nextIndex = alphabet.indexOf(arr[i + 1]);
+
+    if (nextIndex - currIndex !== 1) {
+      return alphabet[currIndex + 1];
+    }
+  }
+}
+
+console.log(findMissingLetter(["a", "b", "c", "d", "f"]));
