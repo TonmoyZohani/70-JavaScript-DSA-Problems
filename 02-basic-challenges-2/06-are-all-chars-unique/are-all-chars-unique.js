2cd8381423774950cbcfcb6e87d39a5c6b4171ca
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-03 20:55:37 +0600

    all characters unique done

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,17 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  let strCode = [];
 
-module.exports = areAllCharactersUnique;
+  for (let i = 0; i < str.length; i++) {
+    strCode.push(str[i].charCodeAt(0));
+  }
+
+  let set = new Set(strCode);
+
+  return [...set].length === str.length;
+}
+
+console.log(areAllCharactersUnique("abcdefg"));
+console.log(areAllCharactersUnique("abcdefgA"));
+console.log(areAllCharactersUnique("programming"));
+console.log(areAllCharactersUnique(""));
+console.log(areAllCharactersUnique("a"));
