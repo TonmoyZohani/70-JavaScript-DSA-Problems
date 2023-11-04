b7cc463926eaf8d35a45cb7243191dba6d4e6fab
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-04 23:29:33 +0600

    calculate total sales problem done

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,19 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, tax) {
+  let sum = 0;
+  const basePrice = products.map((product) => product.price * product.quantity);
 
-module.exports = calculateTotalSalesWithTax;
+  basePrice.forEach((price) => sum += price);
+
+  return sum+(sum*(tax*0.01));
+}
+
+console.log(
+  calculateTotalSalesWithTax(
+    [
+      { name: "Apple", price: 0.5, quantity: 10 },
+      { name: "Banana", price: 0.3, quantity: 20 },
+      { name: "Orange", price: 0.6, quantity: 15 },
+    ],
+    8
+  )
+);
