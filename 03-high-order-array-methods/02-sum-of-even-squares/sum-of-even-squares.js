b7197e1b591bddd4f561a57ebcea0bb875779adf
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-04 23:08:28 +0600

    sum-even-sqares problem done

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,10 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+  const numbers = arr.filter((a) => a % 2 === 0).map((a) => a * a);
 
-module.exports = sumOfEvenSquares;
+  return numbers;
+}
+
+console.log(sumOfEvenSquares([1, 2, 3, 4, 5]));
+console.log(sumOfEvenSquares([-1, 0, 1, 2, 3, 4]));
+console.log(sumOfEvenSquares([]));
+
