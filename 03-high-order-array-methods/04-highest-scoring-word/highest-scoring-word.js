99a258dfed7a5fbe46f8dacdc55641f050d42758
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-05 22:04:20 +0600

    highest scoring problem done

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,21 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  const sentence = str.split(" ");
+  let sum;
 
-module.exports = highestScoringWord;
+  const scores = sentence.map((word) => {
+    let score = 0;
+    for (const letter of word) {
+      score += letter.charCodeAt(0) - 96;
+    }
+    return score;
+  });
+
+  const maxScore = Math.max(...scores);
+  const index = scores.indexOf(maxScore);
+
+  return sentence[index];
+}
+
+console.log(highestScoringWord("man i need a taxi up to ubud"));
+console.log(highestScoringWord("what time are we climbing up the volcano"));
+console.log(highestScoringWord("take me to semynak"));
