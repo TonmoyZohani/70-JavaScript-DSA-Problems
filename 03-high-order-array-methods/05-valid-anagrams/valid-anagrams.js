41beef569af58fdaf30b26624251629dbcb58a4a
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-05 22:15:39 +0600

    anagram problem done

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,14 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  const s1 = str1.split("").sort().join("");
+  const s2 = str2.split("").sort().join("");
 
-module.exports = validAnagrams;
+  if (s1 === s2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(validAnagrams("listen", "silent"));
+console.log(validAnagrams("hello", "world"));
+console.log(validAnagrams("astronomer", "moonstarer"));
