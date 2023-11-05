7cf7e511d6463970da8e2860bd70e7c6fe4428fb
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-05 22:41:13 +0600

    hashgenerator problem done

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,26 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  if (str.trim() === "") {
+    return false;
+  }
 
-module.exports = generateHashtag;
+  const words = str.trim().split(/\s+/);
+  const capitalizedWords = words.map(
+    (word) => word.charAt(0).toUpperCase() + word.slice(1)
+  );
+  const fullSentence = capitalizedWords.join("");
+
+  if (fullSentence.length > 140) {
+    return false;
+  } else {
+    return `#${fullSentence}`;
+  }
+}
+
+console.log(generateHashtag("JavaScript is awesome"));
+console.log(generateHashtag("hello world"));
+console.log(
+  generateHashtag(
+    "This is a very very very very very very very very very very very very very very long input that should result in a false hashtag because it exceeds the character limit of 140"
+  )
+);
+console.log(generateHashtag(""));
