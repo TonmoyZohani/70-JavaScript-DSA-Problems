b42aff272ee35f23bf43f9b7a5d77b1d89006f86
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-06 21:39:17 +0600

    reverse string recursion problem done

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,11 @@
-function reverseString() {}
+function reverseString(str) {
+  if (str.length === 0 || str.length === 1) {
+    return str;
+  }
 
-module.exports = reverseString;
+  return reverseString(str.substring(1)) + str[0];
+}
+
+console.log(reverseString(""));
+console.log(reverseString("a"));
+console.log(reverseString("hello"));
