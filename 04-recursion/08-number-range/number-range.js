ab77186e497041a23c9ad05a89efab4ab09763a5
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-07 22:04:00 +0600

    number range problem done

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,11 @@
-function numberRange() {}
+function numberRange(start, end) {
+  if (start === end) {
+    return [start];
+  }
 
-module.exports = numberRange;
+  const numbers = numberRange(start, end - 1);
+  numbers.push(end);
+  return numbers;
+}
+
+console.log(numberRange(1, 5));
