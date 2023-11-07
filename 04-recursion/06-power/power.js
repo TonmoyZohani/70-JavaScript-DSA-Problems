f690a00fe62da4a50a2d4f14fedfe21da3b7d7d4
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-07 21:45:45 +0600

    power problem done

--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -1,3 +1,11 @@
-function power() {}
+function power(base, n) {
+  if (n === 0) {
+    return 1;
+  }
 
-module.exports = power;
+  return base * power(base, n - 1);
+}
+
+console.log(power(2, 3));
+console.log(power(5, 2));
+console.log(power(3, 4));
