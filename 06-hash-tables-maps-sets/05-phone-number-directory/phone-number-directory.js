5ff50092d79fd7fbcc9328ea1f216d91aebedb73
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-10 19:50:08 +0600

    phone number directory done

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,18 @@
-function phoneNumberDirectory() {}
+const phoneNumbers = [
+  "John:123-456-7890",
+  "Jane:987-654-3210",
+  "Joe:555-555-5555",
+];
 
-module.exports = phoneNumberDirectory;
+function phoneNumberDirectory(numbers) {
+  const map1 = new Map();
+
+  numbers.map((a) => {
+    let phoneArr = a.split(":");
+    map1.set(phoneArr[0], phoneArr[1]);
+  });
+
+  return map1;
+}
+
+console.log(phoneNumberDirectory(phoneNumbers));
