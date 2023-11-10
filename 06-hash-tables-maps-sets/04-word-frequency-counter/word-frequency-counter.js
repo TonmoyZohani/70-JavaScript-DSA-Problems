263da6e5510c47a2f24ce5bd41c7e74e76c3a67c
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-10 19:27:06 +0600

    map word frequency counter

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,18 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  const strArr = str.toLowerCase().split(" ");
+  const map1 = new Map([]);
 
-module.exports = wordFrequencyCounter;
+  for (const item of strArr) {
+    let cnt = map1.get(item) || 0;
+    cnt++;
+    map1.set(item, cnt);
+  }
+
+  return map1;
+}
+
+console.log(
+  wordFrequencyCounter(
+    "The quick quick brown fox jumps over the lazy dog the quick quick"
+  )
+);
