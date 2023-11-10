64cad917a936a7f00631fedd6007f561520d2e87
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-10 20:40:52 +0600

    symmetric difference done

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,12 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+  const setArr1 = new Set(arr1);
+  const setArr2 = new Set(arr2);
 
-module.exports = symmetricDifference;
+  const fullArr = [...setArr1, ...setArr2];
+  const fullArrSet = new Set(fullArr);
+  return [...fullArrSet];
+}
+
+console.log(symmetricDifference([1, 2, 3], [3, 4, 5]));
+console.log(symmetricDifference([1, 2, 3, 4, 5], [5, 4, 3, 2, 1]));
+console.log(symmetricDifference([1, 2, 3], [4, 5, 6]));
