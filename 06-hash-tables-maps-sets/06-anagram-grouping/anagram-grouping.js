d1fb7caef70aaa8410b0d52c5cfe393b3663f332
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-11-10 20:14:40 +0600

    anagram grouping done

--- 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
+++ 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -1,3 +1,22 @@
-function anagramGrouping() {}
+// const anagramGroups = new Map([["student", ["A", "B"]]]);
+// anagramGroups.get("student").push("C");
+// console.log(anagramGroups);
 
-module.exports = anagramGrouping;
+function anagramGrouping(words) {
+  const anagramGroup = new Map();
+
+  for (const word of words) {
+    const sortedWord = word.split("").sort().join("");
+
+    if (anagramGroup.has(sortedWord)) {
+      anagramGroup.get(sortedWord).push(word);
+    } else {
+      anagramGroup.set(sortedWord, [word]);
+    }
+  }
+
+  return Array.from(anagramGroup.values());
+  //   return anagramGroup;
+}
+
+console.log(anagramGrouping(["cat", "act", "dog", "god", "tac"]));
